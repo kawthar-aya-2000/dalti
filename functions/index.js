46b097547e1f46612f46f63a7022257acd032931
Kawthar Aya <kawtharaya2000@gmail.com>
2025-10-01 18:42:40 +0100

    index.js modified

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,23 @@
-const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-admin.initializeApp();
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
-exports.deleteOldAppointments = functions.pubsub
-  .schedule("0 0 * * *") // كل يوم على الساعة 00:00
-  .timeZone("Africa/Algiers") // المنطقة الزمنية الجزائر
-  .onRun(async (context) => {
-    const db = admin.firestore();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
-    const today = new Date();
-    today.setHours(0, 0, 0, 0); // بداية اليوم
+(async () => {
+  const db = admin.firestore();
 
-    const snapshot = await db.collection("appointments")
-      .where("createdAt", "<", admin.firestore.Timestamp.fromDate(today))
-      .get();
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
 
-    const batch = db.batch();
-    snapshot.forEach((doc) => batch.delete(doc.ref));
-    await batch.commit();
+  const snapshot = await db.collection("appointments")
+    .where("createdAt", "<", admin.firestore.Timestamp.fromDate(today))
+    .get();
 
-    console.log("✅ appointments القديمة تم مسحها بنجاح");
-    return null;
-  });
+  const batch = db.batch();
+  snapshot.forEach((doc) => batch.delete(doc.ref));
+  await batch.commit();
 
+  console.log("✅ appointments القديمة تم مسحها بنجاح");
+})();
